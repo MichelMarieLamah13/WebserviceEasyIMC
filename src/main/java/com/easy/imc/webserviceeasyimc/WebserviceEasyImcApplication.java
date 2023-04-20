074dba736d8b6615a236239e789c811643c2f51d
etudiant <lamahmichelmarie@gmail.com>
2023-04-20 14:16:32 +0200

    Initial commit

---
+++ src/main/java/com/easy/imc/webserviceeasyimc/WebserviceEasyImcApplication.java
@@ -0,0 +1,13 @@
+package com.easy.imc.webserviceeasyimc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceEasyImcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserviceEasyImcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easy/imc/webserviceeasyimc/WebserviceEasyImcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.imc.webserviceeasyimc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceEasyImcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
